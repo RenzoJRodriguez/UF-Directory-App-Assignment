c9dee5e5503a3a30be3665be9048f49f2d61d008
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-08-27 19:51:17 -0400

    Created server, wrote JSON file to response and delivered on appropriate URL request

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
 /* Global variables */
 var listingData, server;
 
+// Your request handler should send listingData in the JSON format if a GET request is sent to the '/listings' path.
+// Otherwise, it should send a 404 error.
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  // If the desired url is requested, return a 200 status code and show JSON data
+  if(parsedUrl.pathname === '/listings'){
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.write(listingData);
+    response.end();
+  }
+  // Else, return a 404 status code and show Bad gateway error
+  else {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.write('Bad gateway error');
+    response.end();
+  }
 
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
 };
 
+server = http.createServer(requestHandler);
+
+// This callback function should save the data in the listingData variable, then start the server.
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-   */
+
+    // If there's an error, throw the error
+    if(err){
+      throw err;
+    }
+
+    // Else, set listingData to the data read in from the file
+    listingData = data;
+
+    // Starting server and setting it to listen to port 8080 on localhost
+    server.listen(port, function(){
+      console.log('Server listening on: http://localhost:' + port);
+    });
+
 });
