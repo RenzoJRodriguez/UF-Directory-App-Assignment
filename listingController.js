34d3c9d9c0ac89d1837d27fb3d277d77bacba214
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-09-04 17:23:53 -0400

    Added basic functionality for addition, deletion, and showing details of listings

--- listingController.js
+++ listingController.js
@@ -1,14 +1,31 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      let listing = {
+        "code": this.code,
+        "name": this.name
+      };
+
+      $scope.listings.push(listing);
+
+      this.code = "";
+      this.name = "";
+    };
+
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
+
   }
 ]);
