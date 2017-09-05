e7855a0e9edfc961d06a5cd855ec19dd591c648b
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-09-04 23:19:36 -0400

    Created listing schema w/ pre function which passed mocha test cases, saved json to mongo database, created query functions

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,34 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config.js'),
+    listings = require('./listings.json');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri, { useMongoClient: true });
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
 
+for(let i in listings.entries){
+  let newListing = Listing(listings.entries[i]);
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+  newListing.save(function(err){
+    if(err) throw err;
+
+    console.log('User created');
+  });
+}
+
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,32 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+  code: { type: String, required: true },
+  name: { type: String, required: true },
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  // Gets the current date
+  let currentDate = new Date();
+
+  // Changes the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // If created_at doesn't exist, add it as a field
+  if(!this.created_at){
+    this.created_at = currentDate;
+  }
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
